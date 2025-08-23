09ebd8527e4e68a0ac1f5d2c6f329828756b1675
mersedee <mersede.rahbar@gmail.com>
2025-08-23 16:05:03 +0330

    feat: Implement home page components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import {resumes} from "~/constants";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        {title: "New React Router App"},
+        {name: "description", content: "Welcome to React Router!"},
+    ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar/>
+            <section className="main-section">
+                <div className="page-heading py-16">
+                    <h1>Track Your Applications & Resume Ratings</h1>
+                    <h2>Review your submissions and check AI-powered feedback.</h2>
+                </div>
+            </section>
+
+            {resumes.length > 0 && (
+                <div className="resumes-section">
+                    {resumes.map((resume, index) => (
+                        <ResumeCard key={resume.id} resume={resume}/>
+                    ))}
+                </div>
+            )}
+        </main>
+    );
 }
